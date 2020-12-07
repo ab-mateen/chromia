2a855e78d588eacc84af1de4394e60644054e709
ab-mateen <abmateen1994@gmail.com>
2020-12-07 22:04:13 +0500

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,37 @@
 // Founder Popup
-// Founder Popup 1 Show
-$('.btn-tooltip-1').on('click', function() {
-    $('.founder-popup-container-1').css("display", "inline-block");
-    $('.founder-popup-container-1').css("transition", "display .3s");
-    $('.founder-tooltip-1').css("visibility", "hidden");
-    $('.founder-txt-1').css("position", "unset");
+$(document).ready(function() {
+    // Show Founder Popup 1
+    $('#btn-tooltip1').click(function() {
+            $('#founder-popup1').show("slow", function() {
+                $('.founder-txt-1').css("position", "unset");
+            });
+            $('.founder-tooltip-1').hide("slow");
+        })
+        // Hide Founder Popup 1
+    $('#close-btn1').click(function() {
+            $('.founder-tooltip-1').show("slow");
+            $('#founder-popup1').hide("slow", function() {
+                $('.founder-txt-1').css("position", "absolute");
+            });
+        })
+        // Show Founder Popup 2
+    $('#btn-tooltip2').click(function() {
+            $('#founder-popup2').show("slow");
+            $('.founder-tooltip-2').hide("slow");
+        })
+        // Hide Founder Popup 2
+    $('#close-btn2').click(function() {
+            $('.founder-tooltip-2').show("slow");
+            $('#founder-popup2').hide("slow");
+        })
+        // Show Founder Popup 3
+    $('#btn-tooltip3').click(function() {
+            $('#founder-popup3').show("slow");
+            $('.founder-tooltip-3').hide("slow");
+        })
+        // Hide Founder Popup 3
+    $('#close-btn3').click(function() {
+        $('.founder-tooltip-3').show("slow");
+        $('#founder-popup3').hide("slow");
+    })
 });
-// Founder Popup 2 Show
-$('.btn-tooltip-2').on('click', function() {
-    $('.founder-popup-container-2').css("display", "inline-block");
-    $('.founder-tooltip-2').css("visibility", "hidden");
-});
-// Founder Popup 3 Show
-$('.btn-tooltip-3').on('click', function() {
-    $('.founder-popup-container-3').css("display", "inline-block");
-    $('.founder-tooltip-3').css("visibility", "hidden");
-});
-// Founder Popup Hide
-// Founder Popup 1 Hide
-$('.close').on('click', function() {
-    $('.founder-popup-container-1').css("display", "none");
-    $('.founder-tooltip-1').css("visibility", "visible");
-    $('.founder-txt-1').css("position", "absolute");
-});
-// Founder Popup 2 Hide
-$('.close').on('click', function() {
-    $('.founder-popup-container-2').css("display", "none");
-    $('.founder-tooltip-2').css("visibility", "visible");
-});
-// Founder Popup 3 Hide
-$('.close').on('click', function() {
-    $('.founder-popup-container-3').css("display", "none");
-    $('.founder-tooltip-3').css("visibility", "visible");
-});
-// $(document).ready(function() {
-//     $('.btn-tooltiop').on('click', function() {
-//         $('.founder-popup-container').fadeIn();
-//         $('.founder-tooltip-1').hide();
-//     });
-
-// });
