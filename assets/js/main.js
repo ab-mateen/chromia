2a4859b5778e28d10ea0b31a1921547adae3a93e
ab-mateen <abmateen1994@gmail.com>
2020-12-10 03:25:17 +0500

    responsive page

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,22 +16,30 @@ $(document).ready(function() {
         })
         // Show Founder Popup 2
     $('#btn-tooltip2').click(function() {
-            $('#founder-popup2').show("slow");
+            $('#founder-popup2').show("slow", function() {
+                $('.founder-txt-2').css("position", "unset");
+            });
             $('.founder-tooltip-2').hide("slow");
         })
         // Hide Founder Popup 2
     $('#close-btn2').click(function() {
             $('.founder-tooltip-2').show("slow");
-            $('#founder-popup2').hide("slow");
+            $('#founder-popup2').hide("slow", function() {
+                $('.founder-txt-2').css("position", "absolute");
+            });
         })
         // Show Founder Popup 3
     $('#btn-tooltip3').click(function() {
-            $('#founder-popup3').show("slow");
+            $('#founder-popup3').show("slow", function() {
+                $('.founder-txt-3').css("position", "unset");
+            });
             $('.founder-tooltip-3').hide("slow");
         })
         // Hide Founder Popup 3
     $('#close-btn3').click(function() {
         $('.founder-tooltip-3').show("slow");
-        $('#founder-popup3').hide("slow");
+        $('#founder-popup3').hide("slow", function() {
+            $('.founder-txt-3').css("position", "absolute");
+        });
     })
 });
