1350cc82585006e8f40e21ec0977f7d9b7323f5e
abmateen <abmateen1994@gmail.com>
2020-12-30 05:18:54 +0500

    update code

---
+++ staking/precache-manifest.1975cd22e62245ac66a40736e349d465.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e59fddeb635e469ce0556d03230712c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cf269d8d7bb9906421f0",
+    "url": "./static/css/main.192a6d94.chunk.css"
+  },
+  {
+    "revision": "bf95f0d6cf67a9f24767",
+    "url": "./static/js/2.f93832c0.chunk.js"
+  },
+  {
+    "revision": "5827a1e9c8399201f6f42390db1cd108",
+    "url": "./static/js/2.f93832c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf269d8d7bb9906421f0",
+    "url": "./static/js/main.ac195a91.chunk.js"
+  },
+  {
+    "revision": "be998b72351fa137f528",
+    "url": "./static/js/runtime-main.c1001b72.js"
+  },
+  {
+    "revision": "7a41406a3f032505bde9b3258ee1b889",
+    "url": "./static/media/Battlefin-Black.7a41406a.otf"
+  },
+  {
+    "revision": "da904b1b40e5a15b018cce15d6582822",
+    "url": "./static/media/NB-International-Regular-Webfont.da904b1b.ttf"
+  },
+  {
+    "revision": "da0094c79cd49ac94cb5f7e704ea8bbe",
+    "url": "./static/media/logo.da0094c7.svg"
+  }
+]);
