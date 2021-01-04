10c492a4482e609cea314979e930760b7cc661a4
abmateen <abmateen1994@gmail.com>
2021-01-05 00:31:35 +0500

    update code

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ $('.chromia-slider.owl-carousel').owlCarousel({
     center: true,
     margin: 10,
     URLhashListener: true,
-    autoplayHoverPause: true,
+    autoplayHoverPause: false,
     startPosition: 'URLHash'
 });
 
@@ -16,44 +16,47 @@ $('.chromia-slider.owl-carousel').owlCarousel({
 $(document).ready(function() {
     // Show Founder Popup 1
     $('#btn-tooltip1').click(function() {
-            $('#founder-popup1').show("slow", function() {
-                $('.founder-txt-1').css("position", "unset");
-            });
+            $('#founder-popup1').show("slow");
             $('.founder-tooltip-1').hide("slow");
         })
         // Hide Founder Popup 1
     $('#close-btn1').click(function() {
             $('.founder-tooltip-1').show("slow");
-            $('#founder-popup1').hide("slow", function() {
-                $('.founder-txt-1').css("position", "absolute");
-            });
+            $('#founder-popup1').hide("slow");
         })
         // Show Founder Popup 2
     $('#btn-tooltip2').click(function() {
-            $('#founder-popup2').show("slow", function() {
-                $('.founder-txt-2').css("position", "unset");
-            });
+            $('#founder-popup2').show("slow");
             $('.founder-tooltip-2').hide("slow");
         })
         // Hide Founder Popup 2
     $('#close-btn2').click(function() {
             $('.founder-tooltip-2').show("slow");
-            $('#founder-popup2').hide("slow", function() {
-                $('.founder-txt-2').css("position", "absolute");
-            });
+            $('#founder-popup2').hide("slow");
         })
         // Show Founder Popup 3
     $('#btn-tooltip3').click(function() {
-            $('#founder-popup3').show("slow", function() {
-                $('.founder-txt-3').css("position", "unset");
-            });
+            $('#founder-popup3').show("slow");
             $('.founder-tooltip-3').hide("slow");
         })
         // Hide Founder Popup 3
     $('#close-btn3').click(function() {
         $('.founder-tooltip-3').show("slow");
-        $('#founder-popup3').hide("slow", function() {
-            $('.founder-txt-3').css("position", "absolute");
-        });
+        $('#founder-popup3').hide("slow");
     })
+
+    // Show Founder Popup 3
+    // $('#btn-tooltip3').click(function() {
+    //         $('#founder-popup3').show("slow", function() {
+    //             $('.founder-txt-3').css("position", "unset");
+    //         });
+    //         $('.founder-tooltip-3').hide("slow");
+    //     })
+    // Hide Founder Popup 3
+    //     $('#close-btn3').click(function() {
+    //         $('.founder-tooltip-3').show("slow");
+    //         $('#founder-popup3').hide("slow", function() {
+    //             $('.founder-txt-3').css("position", "absolute");
+    //         });
+    //     })
 });
